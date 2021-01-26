40fbaea7b4d2e625692e02997ea7f20dc0dbd680
Humberto Israel <77642386+humbertoacademic@users.noreply.github.com>
2021-01-25 23:09:31 -0600

    Update operacionReflejo.py

--- src/operacionReflejo.py
+++ src/operacionReflejo.py
@@ -1,4 +1,5 @@
 from senalDiscreta import SenalDiscreta
+from manejadorDeSenales import *
 
 def obtener_reflejo(senal, tipoReflejo):
     if(tipoReflejo==1):     #Para reflejo sobre el eje X
@@ -26,6 +27,18 @@ def obtener_reflejoY(senal):
     inicioAux = - len(datosAux) + 1 - senal.obtener_indice_inicio()
     return SenalDiscreta(datosAux, inicioAux, senal.es_periodica())
 
+def obtener_reflejoHacerTodo(senal, tipoReflejo=2):
+    if(tipoReflejo==1):     #Para reflejo sobre el eje X
+        datosAux = senal.obtener_datos()
+        for i in range(len(datosAux)):
+            datosAux[i] = datosAux[i] * -1 #Voltea la secuencia de valores
+        senalAux = SenalDiscreta(datosAux, senal.obtener_indice_inicio(), senal.es_periodica())
+    elif(tipoReflejo==2):   #Para reflejo sobre el eje y
+        datosAux = senal.obtener_datos()[::-1]
+        inicioAux = - len(datosAux) + 1 - senal.obtener_indice_inicio()
+        senalAux= SenalDiscreta(datosAux, inicioAux, senal.es_periodica())
+    graficarSenalDiscretaDeAudio(senal, senalAux)
+    obtenerAudioDesdeSenalDiscreta(senalAux)
 
 #CODIGO DE PRUEBA:
 # senal1 = SenalDiscreta([1, 2, 3, 4], -2, False)
