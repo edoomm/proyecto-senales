6eb129311910f8f10c482a04941f43df079afe9e
ErickRazielHernandez <44034832+ErickRazielHernandez@users.noreply.github.com>
2021-01-26 02:50:45 -0600

    Se añadieron las funciones de diezmacion e interpolacion para audio (#22)

--- src/operacionInterpolacionDiezmacion.py
+++ src/operacionInterpolacionDiezmacion.py
@@ -1,17 +1,28 @@
 from senalDiscreta import SenalDiscreta
+import matplotlib.pyplot as plt
+from matplotlib import pyplot
+import numpy as np
+from manejadorDeSenales import obtenerAudioDesdeSenalDiscreta
+from manejadorDeSenales import obtenerSenalDiscretaDesdeAudio
 
-#Funcion que calcula la pendiente que es usada para la interpolación
-#Recibe un valor y1, un valor y2 y el factor de interpolacion
 def obtenerPendiente(y1,y2,factor):
+    """
+    Funcion que calcula la pendiente que es usada para la interpolación
+    Recibe un valor y1, un valor y2 y el factor de interpolacion
+    """
     return float((y2-y1)/(factor))
 
-#Esta funcion recibe un numero y devuelve un float con solo un decimal
 def a1decimal(n):
+    """
+    Esta funcion recibe un numero y devuelve un float con solo un decimal
+    """
     return float(int(n*10))/10
 
-#Función que calcula la interpolacion
-#Recibe como parámetros un objeto de tipo señaldiscreta y el factor de interpolacion
 def obtenerInterpolacion(xn,factor):
+    """
+    Función que calcula la interpolacion
+    Recibe como parámetros un objeto de tipo señaldiscreta y el factor de interpolacion
+    """
     x = xn.obtener_datos()
     y = []
     # solo funciona para listas del mismo tamaño
@@ -20,18 +31,18 @@ def obtenerInterpolacion(xn,factor):
         y.append(x[i])
         for j in range(1, factor):
             y.append(a1decimal(float(x[i]+(m*j))))
-    print(y)
     y.append(x[i+1])
     return SenalDiscreta(y, xn.obtener_indice_inicio()*factor, xn.es_periodica())
 
-#Función que calcula la diezmacion
-#Recibe como parámetros un objeto de tipo señaldiscreta y el factor de diezmacion
 def obtenerDiezmacion(xn,factor):
+    """
+    Función que calcula la diezmacion
+    Recibe como parámetros un objeto de tipo señaldiscreta y el factor de diezmacion
+    """
     x = xn.obtener_datos().copy()
     inicio = xn.obtener_indice_inicio()
     origen = xn.obtener_origen()
     index = int(0)
-    print(f"Secuencia: {x}, origen {origen}")
     for i in range(len(x)):
         pointer = inicio+i
         if pointer%factor != 0:
@@ -40,5 +51,22 @@ def obtenerDiezmacion(xn,factor):
                 origen -= 1
         else:
             index += 1
-    print(f"Secuencia: {x}, origen {origen}")
     return SenalDiscreta(x, -origen, xn.es_periodica())
+
+def graficarInterpolacionDiezmacion(newX,resultado,operacion,factor):
+    """
+    Función que imprime dos formas de onda
+    """
+    #x(n) en la primera posicion
+    plt.subplot(211)
+    plt.plot(range(0, len(newX)), newX)
+    plt.ylabel('x(n)')
+
+    #g(n) en la segunda posicion
+    plt.subplot(212)
+    plt.plot(range(0, len(resultado)), resultado)
+    plt.ylabel('g(n)')
+
+    plt.suptitle(operacion+' de x(n)')
+
+    plt.show()
