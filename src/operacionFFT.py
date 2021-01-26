fc51a70147f094248c602063adb42c840489538d
Ian Alvarez <alvarianone@gmail.com>
2021-01-26 04:15:44 -0600

    Añadi operacionFFT

---
+++ src/operacionFFT.py
@@ -0,0 +1,20 @@
+from senalDiscreta import SenalDiscreta
+from scipy.fft import fft, fftfreq, fftshift
+import numpy
+import matplotlib.pyplot as plt
+
+def obtener_FFT(senal): #Obtenemos la FFT de la secuencia
+    datosAux = senal.obtener_datos() #Lista auxiliar donde se guardaran los desplazamientos
+    array = numpy.array(datosAux) #Convertimos esa lista auxiliar a un arreglo de numpy
+    transformada = fft(array) #Obtenemos la fft
+    #graficarFFT(transformada)
+    return SenalDiscreta(transformada.tolist(), senal.obtener_indice_inicio(), senal.es_periodica()) #Retornamos la senial
+
+def graficarFFT(transformada):
+    yf = transformada
+    xf = fftfreq(transformada.size)
+    xf = fftshift(xf)
+    yplot = fftshift(yf)
+    plt.plot(xf, 1 / transformada.size * numpy.abs(yplot))
+    plt.grid()
+    plt.show()
