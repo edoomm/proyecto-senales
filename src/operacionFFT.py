1ae1a6e19a225e6788de42ec9492395df4b6e34d
Ian Alvarez <alvarianone@gmail.com>
2021-01-26 04:28:21 -0600

    Cambio para que complejo muestre hasta 2 decimales

--- src/operacionFFT.py
+++ src/operacionFFT.py
@@ -7,6 +7,7 @@ def obtener_FFT(senal): #Obtenemos la FFT de la secuencia
     datosAux = senal.obtener_datos() #Lista auxiliar donde se guardaran los desplazamientos
     array = numpy.array(datosAux) #Convertimos esa lista auxiliar a un arreglo de numpy
     transformada = fft(array) #Obtenemos la fft
+    transformada = numpy.around(transformada, 2)
     #graficarFFT(transformada)
     return SenalDiscreta(transformada.tolist(), senal.obtener_indice_inicio(), senal.es_periodica()) #Retornamos la senial
 
